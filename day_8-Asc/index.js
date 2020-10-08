1389005a16ccc1d42b165bf8f2282e910da49ebb
deadania14 <deadania14@gmail.com>
2020-10-08 15:32:31 +0700

    perbaikan

--- day_8-Asc/index.js
+++ day_8-Asc/index.js
@@ -2,12 +2,41 @@
 var readBooks = require('./callback.js')
 
 var books = [
-    { name: 'LOTR', timeSpent: 3000 },
-    { name: 'Fidas', timeSpent: 2000 },
-    { name: 'Kalkulus', timeSpent: 4000 }
-]
+        { name: 'LOTR', timeSpent: 3000 },
+        { name: 'Fidas', timeSpent: 2000 },
+        { name: 'Kalkulus', timeSpent: 4000 }
+    ]
+    // Tulis code untuk memanggil
+    // function readBooks di sini(salah)
+    // books.forEach(e => readBooks(10000, e, (callbackFn) => {
+    //     console.log(callbackFn);
+    // }))
 
-// Tulis code untuk memanggil function readBooks di sini
-books.forEach(e => readBooks(10000, e, (callbackFn) => {
-    console.log(callbackFn);
-}))
+function baca(time, tbooks) {
+    if (tbooks.length === 0) {
+        console.log("Tidak ada buku yang bisa dibaca lagi");
+    } else {
+        readBooks(time, tbooks[0], sisaWaktu => {
+            return baca(sisaWaktu, tbooks.slice(1))
+        })
+
+    }
+}
+
+baca(10000, books)
+
+
+// DARI TUTOR:
+// let t = 10000
+// let i = 0;
+
+// function panggil() {
+//     readBooks(t, books[i], function(sisawaktu) {
+//         t = sisawaktu
+//         i++
+//         if (i < books.length)
+//             panggil()
+//     })
+// }
+
+// panggil();
--- day_8-Asc/index2.js
+++ day_8-Asc/index2.js
@@ -8,8 +8,39 @@ var books = [
 
 // Lanjutkan code untuk menjalankan function readBooksPromise
 
-books.forEach(e => readBooksPromise(10000, e).then(function(fullfilled) {
-    console.log(fullfilled);
-}).catch(function(error) {
-    console.log(error);
-}))
+// books.forEach(e => readBooksPromise(10000, e).then(function(fullfilled) {
+//     console.log(fullfilled);
+// }).catch(function(sisaWaktu) {
+//     console.log(sisaWaktu);
+// }))
+
+// async function asyncCall() {
+//     let t = 10000
+//     for (let i = 0; i < books.length; i++) {
+//         t = await readBooksPromise(t, books[i]).then(function(sisaWaktu) {
+//                 return sisaWaktu;
+//             })
+//             .catch(function(sisaWaktu) {
+//                 return sisaWaktu;
+//             })
+//     }
+//     console.log("selesai")
+// }
+
+// asyncCall();
+
+async function callPromise(time, bookarr) {
+    if (bookarr.length === 0) {
+        console.log("Tidak ada buku yang bisa dibaca lagi");
+    } else {
+
+        time = await readBooksPromise(time, bookarr[0]).then(function(sisaWaktu) {
+                return callPromise(sisaWaktu, bookarr.slice(1));
+            })
+            .catch(function(sisaWaktu) {
+                return sisaWaktu;
+            })
+    }
+}
+
+callPromise(10000, books);
