ad9e92693030047d95f33598e79c8fba11f20652
deadania14 <deadania14@gmail.com>
2020-10-16 01:11:51 +0700

    Tugas day 14 no.2

--- sanbercodeimrn0920/SanberApp/tugas/tugas14/App.js
+++ sanbercodeimrn0920/SanberApp/tugas/tugas14/App.js
@@ -1,10 +1,15 @@
 import React from 'react';
 import Main from './components/Main';
+import SkillScreen from './components/skillScreen';
 
 export default class Tugas14 extends React.Component {
   render() {
     return (
-      <Main />
+      <>
+      {/* <Main/> */}
+      <SkillScreen />
+      
+      </>
     )
   }
 }
